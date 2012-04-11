3f9a22ca96844a26dda359d5e3c2bd90e9ee9a99
Damian Bushong <damian@emberlabs.org>
2012-04-10 20:50:24 -0500

    Heavily tweaking the PostSearchController, to add more extensive tag filtering.

--- app/src/codebite/homebooru/Controller/PostSearchController.php
+++ app/src/codebite/homebooru/Controller/PostSearchController.php
@@ -9,14 +9,69 @@ if(!defined('SHOT_ROOT')) exit;
 class PostSearchController
 	extends ObjectController
 {
+	const SEARCH_MAX = 20;
+
 	public function runController()
 	{
-		$max = 20;
+		$max = self::SEARCH_MAX;
 
 		$tags = $this->request->getInput('REQUEST::q', '');
 
-		preg_match_all('#\w+[\w\(\)]*#i', $tags, $_tags);
-		$tags = array_unique(array_shift($_tags));
+		$tags = explode(' ', $tags);
+
+		// sort out "normal" tags
+		$normal_tags = preg_grep('#^\-?[\w]+[\w\(\)]*$#i', $tags);
+		$_normal_tags = array_unique($normal_tags);
+
+		// sort out magical "parameter" tags
+		$param_tags = preg_grep('#^\-?[\w]+[\w\(\)]+\:[\w\.\(\)]*$#i', $tags);
+		$_param_tags = array_unique($param_tags);
+
+		// nuke out the old variables for resorting in a bit
+		unset($normal_tags, $param_tags);
+
+		$whitelist_params = array(
+			'source',
+			'rating',
+			'id',
+		);
+
+		$exclude_normal_tags = $normal_tags = $param_tags = $exclude_param_tags = array();
+		foreach($_param_tags as $tag)
+		{
+			$exclude = false;
+			list($param, $value) = explode(':', $tag, 2);
+
+			if($param[0] == '-')
+			{
+				$param = substr($param, 1);
+				$exclude = true;
+			}
+			if(in_array($param, $whitelist_params))
+			{
+				if($exclude)
+				{
+					$param_tags[] = array($param, $value);
+				}
+				else
+				{
+					$exclude_param_tags[] = array($param, $value);
+				}
+			}
+		}
+		foreach($_normal_tags as $tag)
+		{
+			if($tag[0] == '-')
+			{
+				$normal_tags[] = substr($tag, 1);
+			}
+			else
+			{
+				$exclude_normal_tags[] = $tag;
+			}
+		}
+
+		// @todo handle tag use properly
 
 		$beans = R::taggedAll('post', $tags);
 
