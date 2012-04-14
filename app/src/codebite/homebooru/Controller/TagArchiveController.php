42cc0a261a2db72fcd03072c584de8757ca505b1
Damian Bushong <damian@emberlabs.org>
2012-04-13 23:08:27 -0500

    Add new view-tag page

---
+++ app/src/codebite/homebooru/Controller/TagArchiveController.php
@@ -0,0 +1,30 @@
+<?php
+namespace codebite\homebooru\Controller;
+use \codebite\homebooru\Model\BooruPostModel;
+use \codebite\homebooru\Model\BooruTagModel;
+use \emberlabs\shot\Controller\ObjectController;
+use \R;
+
+if(!defined('SHOT_ROOT')) exit;
+
+class TagArchiveController
+	extends ObjectController
+{
+	public function runController()
+	{
+		$max = 100;
+		$page = $this->request->getRoute()->get('page') ?: 1;
+		$beans = R::findAll('tag', 'ORDER BY title ASC LIMIT ?', array($max));
+
+		$this->app->form->setFormSeed($this->app->session->getSessionSeed());
+		$this->response->setBody('viewtags.twig.html');
+		$this->response->setTemplateVars(array(
+			'page'				=> array(
+				'tags'				=> true,
+			),
+			'tags'				=> $beans,
+		));
+
+		return $this->response;
+	}
+}
