73978da5a8d3d41752de0d26afece05c59775499
Damian Bushong <katana@codebite.net>
2012-05-01 00:47:27 -0500

    Beginning work on magic caching

--- app/src/codebite/homebooru/Runtime/Listeners.php
+++ app/src/codebite/homebooru/Runtime/Listeners.php
@@ -1,6 +1,7 @@
 <?php
 namespace codebite\homebooru\Runtime;
 use \codebite\homebooru\WebKernel as App;
+use \codebite\homebooru\Controller\CacheController;
 use \emberlabs\openflame\Event\Dispatcher;
 use \emberlabs\openflame\Event\Instance as Event;
 use \R;
@@ -22,3 +23,31 @@ $app->dispatcher->register('shot.hook.runtime.render.post', 15, function(Event $
 	);
 	$event->setData(array(str_replace($search, $replace, reset($event->getData()))));
 });
+
+/**
+ * caching integration
+ *
+ *  - check if cacheable, then see if cached - if so, overload with CacheController and appropriate cache entry name
+ *
+$app->dispatcher->register('shot.hook.runtime.runcontroller', 0, function(Event $event) use ($app) {
+	// extract controller
+	$controller = reset($event->getData());
+
+	if($controller->isCacheable())
+	{
+		if(false)
+		{
+			$cache_binds = $controller->getCacheBinds();
+
+			$controller = new CacheController($app, $app->request, $app->response);
+			$controller->setCacheBinds($cache_binds);
+		}
+	}
+});
+ */
+
+/**
+ * caching integration
+ *
+ *  - check if cacheable, then cache if so.
+ */
